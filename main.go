493fde6c1810dfc5af2494133362d6ed297c5ce2
yuriyskrypnyk <skrypnyk81@gmail.com>
2023-05-08 08:58:33 +0200

    Change github url

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/Skrypnyk81/Dev-ops-prometheus/cmd"
+import "github.com/Skrypnyk81/dev-ops-prometheus/cmd"
 
 func main() {
 	cmd.Execute()
