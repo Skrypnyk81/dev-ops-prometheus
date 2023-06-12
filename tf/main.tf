d8da8d186b929d18937148e47c6b28873f224531
Yuriy Skrypnyk <skrypnyk@hotmail.it>
2023-06-12 20:41:57 +0200

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -1,5 +1,5 @@
 module "gke_cluster" {
-  source         = "github.com/Skrypnyk81/dev-ops-prometheus/tf-google-gke-cluster"
+  source         = "github.com/Skrypnyk81/dev-ops-prometheus/tf"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = 2
