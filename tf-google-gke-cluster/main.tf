34d0103ceff2af80330c5d2fe6c285f7a53a43a9
Yuriy Skrypnyk <skrypnyk@hotmail.it>
2023-06-12 20:35:15 +0200

    Delete tf-google-gke-cluster directory

--- tf-google-gke-cluster/main.tf
+++
@@ -1,6 +0,0 @@
-module "gke_cluster" {
-  source         = "github.com/Skrypnyk81/dev-ops-prometheus/tf-google-gke-cluster"
-  GOOGLE_REGION  = var.GOOGLE_REGION
-  GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 2
-}
--- tf-google-gke-cluster/vars.tfvars
+++
@@ -1,3 +0,0 @@
-GOOGLE_REGION  = "europe-west12-a"
-GOOGLE_PROJECT = "Bitcoin Bot"
-GKE_NUM_NODES  = 3
