ae5086db2d7755157c7f8594904bffcf40e8934a
Yuriy Skrypnyk <skrypnyk81@gmail.com>
2023-06-13 07:09:59 +0200

    Add terraform file

--- tf-google-gke-cluster/main.tf
+++ tf-google-gke-cluster/main.tf
@@ -1,6 +1,12 @@
-module "gke_cluster" {
-  source         = "github.com/Skrypnyk81/dev-ops-prometheus/tf-google-gke-cluster"
-  GOOGLE_REGION  = var.GOOGLE_REGION
-  GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = 2
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.52.0"
+    }
+  }
+}
+provider "google" {
+  project = var.GOOGLE_PROJECT
+  region  = var.GOOGLE_REGION
 }
